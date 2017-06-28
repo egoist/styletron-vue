fef0a6bd88be20a8f634c12a8be5a87c8ff4ceaa
EGOIST <0x142857@gmail.com>
2017-06-28 23:49:04 +0800

    update example

--- example/index.js
+++ example/index.js
@@ -1,23 +1,53 @@
 import Vue from 'vue'
 import Styletron from 'styletron'
+import GitHub from 'vue-github-badge'
 import { styled } from '../src'
 
 const App = {
   name: 'app',
+  data() {
+    return {
+      fontSize: 12
+    }
+  },
+  methods: {
+    handleFontSize(e) {
+      this.fontSize = e.target.value
+    }
+  },
   render() {
-    const Button = styled('button', {
+    const Button = styled('button', props => ({
       color: 'pink',
-      fontSize: '20px',
+      fontSize: `${props.fontSize}px`,
       border: '1px solid #e2e2e2',
-      background: 'white'
-    })
+      background: 'white',
+      marginRight: '10px',
+      padding: '10px 20px'
+    }))
+
     const RedButton = styled(Button, {
-      color: 'red'
+      color: 'red',
+      ':hover': {
+        color: 'white',
+        background: 'red'
+      }
     })
+
     return (
       <div>
-        <Button>hi</Button>
-        <RedButton>there</RedButton>
+        <GitHub slug="egoist/styletron-vue" />
+        <label>
+          Font Size:{' '}
+          <input
+            value={this.fontSize}
+            type="number"
+            onInput={this.handleFontSize}
+          />
+        </label>
+        <p>
+          <Button fontSize={this.fontSize}>hi</Button>
+          <RedButton fontSize={this.fontSize}>move mouse over here</RedButton>
+        </p>
       </div>
     )
   }
